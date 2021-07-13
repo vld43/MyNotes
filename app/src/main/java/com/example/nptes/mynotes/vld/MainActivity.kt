fa1d058c320120d6ea4913e587f961dd736760fc
vova4 <435335vova@gmail.com>
2021-07-13 18:48:18 +0300

    Инициализация объектов в MainActivity.

--- app/src/main/java/com/example/nptes/mynotes/vld/MainActivity.kt
+++ app/src/main/java/com/example/nptes/mynotes/vld/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.nptes.mynotes.vld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.nptes.mynotes.vld.databinding.ActivityMainBinding
+import com.example.nptes.mynotes.vld.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolBar: Toolbar
+    lateinit var navController: NavController
+
+    private var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        APP_ACTIVITY = this
+
+        toolBar = binding.toolbar
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setSupportActionBar(toolBar)
+        title = getString(R.string.title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/example/nptes/mynotes/vld/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.example.nptes.mynotes.vld.utilits
+
+import com.example.nptes.mynotes.vld.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity

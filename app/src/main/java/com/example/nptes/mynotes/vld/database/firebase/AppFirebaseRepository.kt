1ba94ab944515f3267a6ece6217980ce368a4b7c
vova4 <435335vova@gmail.com>
2021-07-15 13:43:17 +0300

    Реализация создания новой заметки в Firebase.

--- app/src/main/java/com/example/nptes/mynotes/vld/database/firebase/AppFirebaseRepository.kt
+++ app/src/main/java/com/example/nptes/mynotes/vld/database/firebase/AppFirebaseRepository.kt
@@ -3,18 +3,31 @@ package com.example.nptes.mynotes.vld.database.firebase
 import androidx.lifecycle.LiveData
 import com.example.nptes.mynotes.vld.database.DatabaseRepository
 import com.example.nptes.mynotes.vld.models.AppNote
-import com.example.nptes.mynotes.vld.utilits.EMAIL
-import com.example.nptes.mynotes.vld.utilits.PASSWORD
+import com.example.nptes.mynotes.vld.utilits.*
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class AppFirebaseRepository : DatabaseRepository {
 
     private val auth = FirebaseAuth.getInstance()
+    private val databaseReference = FirebaseDatabase.getInstance()
+        .reference
+        .child(auth.currentUser?.uid.toString())
 
     override val allNotes: LiveData<List<AppNote>> = AllNoteLiveData()
 
     override suspend fun insert(note: AppNote, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        val idNote = databaseReference.push().key.toString()
+        val mapNote = hashMapOf<String, Any>()
+
+        mapNote[ID_FIREBASE] = idNote
+        mapNote[NAME] = note.name
+        mapNote[TEXT] = note.text
+
+        databaseReference.child(idNote)
+            .updateChildren(mapNote)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { showToast(it.message.toString()) }
     }
 
     override suspend fun delete(note: AppNote, onSuccess: () -> Unit) {
--- app/src/main/java/com/example/nptes/mynotes/vld/utilits/constants.kt
+++ app/src/main/java/com/example/nptes/mynotes/vld/utilits/constants.kt
@@ -6,9 +6,14 @@ import com.example.nptes.mynotes.vld.database.DatabaseRepository
 lateinit var APP_ACTIVITY: MainActivity
 lateinit var REPOSITORY: DatabaseRepository
 
+lateinit var EMAIL: String
+lateinit var PASSWORD: String
+
 const val TYPE_DATABASE = "type_database"
 const val TYPE_ROOM = "type_room"
 const val TYPE_FIREBASE = "type_firebase"
 
-lateinit var EMAIL: String
-lateinit var PASSWORD: String
+const val ID_FIREBASE = "id_firebase"
+const val NAME = "name"
+const val TEXT = "text"
+
